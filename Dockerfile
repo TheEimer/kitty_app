87fb5df542264ed70dd5c68bb1f7124e83be2f4d
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-02-24 17:52:58 +0100

    switch port

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.8.1-slim 
 
 ENV *PYTHONUNBUFFERED *1 
-EXPOSE 8080 
+EXPOSE 8000 
 WORKDIR /todo_list # Make /app as a working directory in the container
 
 COPY ./requirements.txt .
--- todo_list.py
+++ todo_list.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
     uvicorn.run(
         todo_list,
         host="0.0.0.0",
-        port=8080,
+        port=8000,
         log_level=os.getenv("LOG_LEVEL", "info"),
         # proxy_headers=True,
     )
